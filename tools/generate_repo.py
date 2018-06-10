1681087c1e12ac859dcbb277e1f203b3120567cc
Nikolay Voronov <nkvoronov@gmail.com>
2018-06-10 20:13:42 +0300

    reinit

--- tools/generate_repo.py
+++ tools/generate_repo.py
@@ -32,7 +32,8 @@ class Generator:
         self.config.read('config.ini')
         
         self.gitcomment = "Update to version " + self.config.get('addon', 'version')
-        self.resources_path = "resources"        
+        self.isrevision = self.config.get('options', 'is_revision')
+        self.resources_path = "src"        
         self.tools_path=os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__))))
         self.rev_path = self.tools_path + os.path.sep + "revision.txt"
         self.output_path=self.config.get('locations', 'output_path')
@@ -44,7 +45,7 @@ class Generator:
         self._pre_run()
         self._generate_repo_files()
         self._generate_addons_file()
-        self._generate_md5_file()
+        self._generate_md5_file(self.output_path + "addons.xml")
         self._generate_zip_files()        
         # notify user
         print "Finished updating addons xml, md5 files and zipping addons"
@@ -83,12 +84,17 @@ class Generator:
         else:
             self.revision = 1
 
-        if self.revision < 100 and self.revision > 10:        
+        if self.revision > 99:
+            self.revision_str = str(self.revision)        
+        elif self.revision <= 99 and self.revision > 9:        
             self.revision_str = "0" + str(self.revision)
         else:
             self.revision_str = "00" + str(self.revision)
-            
-        self.gitcomment = self.gitcomment + "." + self.revision_str
+
+        if self.isrevision == "true":                
+            self.gitcomment = self.gitcomment + "." + self.revision_str
+        else:
+            self.gitcomment = self.gitcomment + " (" + self.revision_str + ")"
             
         print "########## " + self.gitcomment + " ##########"
         
@@ -111,24 +117,31 @@ class Generator:
         if os.path.isfile( self.rev_path ):
             os.remove(self.rev_path)
             
-        rev_file = open(self.rev_path, "w")
-        rev_file.write(str(self.revision))
-        rev_file.close()
-        
+        self._save_file(str(self.revision), self.rev_path)
+                    
         self._push_to_git()
 
     def _generate_repo_files ( self ):
         
         addonid=self.config.get('addon', 'id')
         name=self.config.get('addon', 'name')
-        version=self.config.get('addon', 'version') + "." + self.revision_str      
+        if self.isrevision == "true":
+            version=self.config.get('addon', 'version') + "." + self.revision_str
+        else:
+            version=self.config.get('addon', 'version')        
         author=self.config.get('addon', 'author')
+        kodi=self.config.get('addon', 'kodi')
         summary_en=self.config.get('addon', 'summary_en') 
         summary_ru=self.config.get('addon', 'summary_ru')
         description_en=self.config.get('addon', 'description_en')
         description_ru=self.config.get('addon', 'description_ru')
         news=self.config.get('addon', 'news')
-        url=self.config.get('locations', 'url')      
+        branch=self.config.get('locations', 'branch')
+        url=self.config.get('locations', 'url')
+        datadir=self.config.get('locations', 'datadir')
+        compressed=self.config.get('options', 'compressed')
+        zip=self.config.get('options', 'zip')
+        hashes=self.config.get('options', 'hashes') 
 
         if os.path.isfile(addonid + os.path.sep + "addon.xml"):return
         
@@ -138,39 +151,40 @@ class Generator:
             template_xml=template.read()
         
         repo_xml = template_xml.format(
-            addonid= addonid,
+            addonid=addonid,
             name=name,
             version=version,
             author=author,
+            kodi=kodi,
             summary_en=summary_en,
             summary_ru=summary_ru,
             description_en=description_en,
             description_ru=description_ru,
             news=news,
+            branch=branch,
             url=url,
+            datadir=datadir,
+            compressed=compressed,
+            zip=zip,            
+            hashes=hashes,
             output_path=self.output_path)
-        
+            
+        if os.path.exists(self.resources_path):
+            shutil.copytree(os.path.abspath(self.resources_path), os.path.abspath(addonid))
+            
         # save file
         if not os.path.exists(addonid):
             os.makedirs(addonid)
             
         self._save_file( repo_xml, file=addonid + os.path.sep + "addon.xml" )
-        
-        if os.path.exists(self.resources_path):
-            files = os.listdir( "." + os.path.sep + self.resources_path)
-            for file in files:
-                shutil.copyfile(self.resources_path + os.path.sep + file, addonid + os.path.sep + file)
-        
+                        
     def _generate_zip_files ( self ):
         addons = os.listdir( "." )
         # loop thru and add each addons addon.xml file
         for addon in addons:
             # create path
             _path = os.path.join( addon, "addon.xml" )
-            
-            _icon = os.path.join( addon, "icon.png" )
-            _fanart = os.path.join( addon, "fanart.jpg" )
-            
+                       
             #skip path if it has no addon.xml
             if not os.path.isfile( _path ): continue       
             try:
@@ -180,15 +194,14 @@ class Generator:
                 _path = os.path.join( addon, "addon.xml" )
                 # split lines for stripping
                 document = minidom.parse(_path)
-                for parent in document.getElementsByTagName("addon"):
+                addon_elements = document.getElementsByTagName("addon")
+                for parent in addon_elements:
                     version = parent.getAttribute("version")
                     addonid = parent.getAttribute("id")
+                                                                        
                 self._generate_zip_file(addon, version, addonid)
-                
-                if os.path.isfile( _icon ):
-                    shutil.copyfile(_icon, self.output_path + addonid + os.path.sep + "icon.png")
-                if os.path.isfile( _fanart ):
-                    shutil.copyfile(_fanart, self.output_path + addonid + os.path.sep + "fanart.jpg")
+                                
+                self._copy_resources(addonid, document)
                     
             except Exception, e:
                 print e
@@ -208,9 +221,13 @@ class Generator:
             if not os.path.exists(self.output_path + addonid):
                 os.makedirs(self.output_path + addonid)
          
-            if os.path.isfile(self.output_path + addonid + os.path.sep + filename):
-                os.rename(self.output_path + addonid + os.path.sep + filename, self.output_path + addonid + os.path.sep + filename + "." + datetime.datetime.now().strftime("%Y%m%d%H%M%S") )
             shutil.move(filename, self.output_path + addonid + os.path.sep + filename)
+            
+            zip_md5=self.config.get('options', 'zip_md5')
+            
+            if zip_md5 == "true":
+                self._generate_md5_file(self.output_path + addonid + os.path.sep + filename)
+            
         except Exception, e:
             print e
 
@@ -246,12 +263,12 @@ class Generator:
         # save file
         self._save_file( addons_xml.encode( "utf-8" ), file=self.output_path + "addons.xml" )
 
-    def _generate_md5_file( self ):
+    def _generate_md5_file( self, pfile ):
         try:
             # create a new md5 hash
-            m = md5.new( open(self.output_path +  "addons.xml" ).read() ).hexdigest()
+            m = md5.new( open(pfile).read() ).hexdigest()
             # save file
-            self._save_file( m, file=self.output_path + "addons.xml.md5" )
+            self._save_file( m, file=pfile + ".md5" )
         except Exception, e:
             # oops
             print "An error occurred creating addons.xml.md5 file!\n%s" % ( e, )
@@ -263,8 +280,36 @@ class Generator:
         except Exception, e:
             # oops
             print "An error occurred saving %s file!\n%s" % ( file, e, )
-
-
+            
+    def _copy_resources( self, addonid, document):
+    
+        assets_elements = document.getElementsByTagName("assets")
+    
+        if len(assets_elements)>0:
+        
+            for parent in assets_elements:
+                for node in parent.childNodes:
+                    res_patch = node.toxml().replace('<icon>','').replace('</icon>','').replace('<fanart>','').replace('</fanart>','').replace('<screenshot>','').replace('</screenshot>','').strip()
+                    if res_patch != "":
+                        src_patch = os.path.join( addonid, res_patch )
+                        dst_patch = os.path.join(os.path.join( self.output_path, addonid ), res_patch)
+                        #dst_patch = os.path.join(os.path.join( self.output_path, addonid ), os.path.basename(res_patch))
+                        if os.path.isfile( src_patch ):
+                            if not os.path.exists(os.path.dirname(dst_patch)):
+                                os.makedirs(os.path.dirname(dst_patch))
+                            shutil.copyfile(src_patch, dst_patch)
+                     
+        else:
+        
+            icon = os.path.join( addonid, "icon.png" )
+            fanart = os.path.join( addonid, "fanart.jpg" )                
+            res_path = os.path.join( self.output_path, addonid )
+            
+            if os.path.isfile( icon ):
+                shutil.copyfile(icon, os.path.join(res_path, "icon.png"))
+            if os.path.isfile( fanart ):
+                shutil.copyfile(fanart, os.path.join(res_path,"fanart.jpg"))
+            
 if ( __name__ == "__main__" ):
     # start
     Generator()
